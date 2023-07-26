423ce562845af39e04d21468ac9729b05530aecf
Андрей <97339527+rarinaferrari@users.noreply.github.com>
2023-07-26 15:00:15 +0300

    Update checkmail.py

--- checkmail.py
+++ checkmail.py
@@ -1,25 +1,33 @@
 import smtplib
+import logging
 from email.mime.text import MIMEText
 
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
 smtp_server = 'mail.dvpsrf.space'
-smtp_port = 25
+smtp_port = 587  # Используем порт 587 для SMTP Submission
 
-from_email = 'user1@dvpsrf.space'
-to_email = 'user2@dvpsrf.space'
+sender = 'user1@dvpsrf.space'
+recipient = 'user2@dvpsrf.space'
+message_text = "Test email from Python."
 
-subject = 'Test Email'
-body = 'This is a test email sent from Python.'
-
-msg = MIMEText(body)
-msg['Subject'] = subject
-msg['From'] = from_email
-msg['To'] = to_email
+msg = MIMEText(message_text)
+msg['Subject'] = 'Test Email'
+msg['From'] = sender
+msg['To'] = recipient
 
 try:
-    smtp = smtplib.SMTP(smtp_server, smtp_port)
-    smtp.sendmail(from_email, to_email, msg.as_string())
-    smtp.quit()
+    server = smtplib.SMTP(smtp_server, smtp_port)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
 
-    print("Test email sent successfully.")
+    # server.login('username', 'password')
+
+    server.sendmail(sender, recipient, msg.as_string())
+    logging.info("Test email sent successfully.")
 except Exception as e:
-    print("Error sending test email:", e)
+    logging.error(f"Error sending test email: {e}")
+finally:
+    server.quit()
+
