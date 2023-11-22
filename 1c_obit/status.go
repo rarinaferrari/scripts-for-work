f037967e009bdd7dcc2f44a0fe147bda85251969
Андрей <97339527+rarinaferrari@users.noreply.github.com>
2023-11-22 15:47:23 +0300

    Update status.go

--- 1c_obit/status.go
+++ 1c_obit/status.go
@@ -1,59 +1,83 @@
-	package main
+package main
 
-	import (
-		"fmt"
-		"io"
-		"log"
-		"net/http"
-		"os"
-		"strings"
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
 
-		"github.com/joho/godotenv"
-	)
+	"github.com/joho/godotenv"
+    	"github.com/rs/zerolog/log"
+)
 
-	func main() {
-		err := godotenv.Load(".env")
-		if err != nil {
-			log.Fatal("Error loading .env file")
-			os.Exit(1)
-		}
-
-		const serverPort = 8080
-		serverURL := os.Getenv("SERVER_URL")
-		if serverURL == "" {
-			log.Fatal("SERVER_URL must be set in .env file")
-		}
-
-		requestURL := fmt.Sprintf("http://%s:%d", serverURL, serverPort)
-		fmt.Println("Server URL:", requestURL)
-		if isServerAvailable(requestURL) {
-			log.Println("Server is available")
-
-		}
-
-		log.Println("Server is not available")
+func main() {
+	err := godotenv.Load("../.env")
+	if err != nil {
+		log.Fatal().Err(err).Msg("Error loading .env file")
 		os.Exit(1)
 	}
 
-	func isServerAvailable(url string) bool {
-		response, err := http.Get(url)
-		if err != nil {
-			log.Println("Error making request:", err)
-			return false
-		}
+	serverURL := os.Getenv("SERVER_URL")
+	alias := os.Getenv("DB_ALIAS")
+	serverPort := os.Getenv("SERVER_PORT")
 
-		defer response.Body.Close()
-
-		if response.StatusCode >= 200 && response.StatusCode < 300 {
-			body, err := io.ReadAll(response.Body)
-			if err != nil {
-				log.Println("Error reading response body:", err)
-				return false
-			}
-
-			return strings.Contains(string(body), "1С:ПРЕДПРИЯТИЕ") || strings.Contains(string(body), "1С:Предприятие")
-		}
-
-		log.Printf("Received non-success status code: %d\n", response.StatusCode)
-		return false
+	if serverURL == "" || alias == "" || serverPort == "" {
+		log.Error().Err(err).Msg("SERVER_URL, ALIAS_URL, SERVER_PORT must be set in .env file")
 	}
+
+	requestURL := fmt.Sprintf("http://%s:%s/%s", serverURL, serverPort, alias)
+	fmt.Println("Server URL:", requestURL)
+
+	responseBody, err := isServerAvailable(requestURL)
+	if err != nil {
+		log.Error().Err(err).Msg("Error checking server availability")
+		os.Exit(1)
+	}
+
+	if responseBody {
+		log.Info().Msg("Server is available")
+	} else {
+		log.Error().Err(err).Msg("Server is not available")
+		os.Exit(1)
+	}
+}
+
+
+func isServerAvailable(url string) (bool, error) {
+	response, err := http.Get(url)
+	if err != nil {
+		log.Logger.Error().Err(err).Msg("Error making request")
+		return false, fmt.Errorf("error making request: %v", err)
+	}
+	defer response.Body.Close()
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		log.Logger.Error().Err(err).Msg("Error reading response body")
+		return false, fmt.Errorf("error reading response body: %v", err)
+	}
+
+	responseBody := string(body)
+
+	switch response.StatusCode {
+	case http.StatusOK, http.StatusCreated:
+		if strings.Contains(responseBody, "1C:Enterprise") || strings.Contains(responseBody, "1C:Предприятие") {
+			return true, nil
+		}
+		
+		return false, nil
+
+	case http.StatusUnauthorized, http.StatusForbidden:
+		log.Logger.Info().Int("statusCode", response.StatusCode).Msg("Server requires authentication")
+		return true, nil //Вернул true, ибо ситуация ожидаема
+	default:
+		log.Logger.Info().
+		Int("statusCode", response.StatusCode).
+		Str("responseBody", responseBody).
+		Msg("Received non-success status code")
+
+		return false, fmt.Errorf("received non-success status code: %d", response.StatusCode)
+
+	}
+}
